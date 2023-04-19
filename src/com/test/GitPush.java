e0b5879bb2f4ba289577086dfda034b068de3583
ramachandran9789 <ramki.ramachandran@gmail.com>
2023-04-19 11:38:56 +0530

    hi

---
+++ src/com/test/GitPush.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class GitPush {
+
+	private void push() {
+
+		System.out.println("git push 1");
+	}
+
+	public static void main(String[] args) {
+
+		GitPush push = new GitPush();
+		push.push();
+
+		System.out.println("Git Push");
+
+	}
+
+}
